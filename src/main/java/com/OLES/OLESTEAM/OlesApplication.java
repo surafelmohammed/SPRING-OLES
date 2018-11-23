9464c5b24182817bb97901dfe88cebf0a4b6f4a0
zekaryas <zekaryasdinku@gmail.com>
2018-11-23 23:07:20 +1000

    first commit

---
+++ src/main/java/com/OLES/OLESTEAM/OlesApplication.java
@@ -0,0 +1,12 @@
+package com.OLES.OLESTEAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlesApplication.class, args);
+	}
+}
